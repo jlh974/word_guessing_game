e8d5f5cdb5f19cb9e3b958b06f2e01880eb12a7f
Jeannine Hall <jeannine.hall1815@gmail.com>
2022-04-06 21:16:21 -0500

    letter guesses working

--- word_guessing_game_exercise.py
+++ word_guessing_game_exercise.py
@@ -1,9 +1,31 @@
-correct_word = 'difference'
-is_guessed = False
-while is_guessed == False:
-    guess_word = str(input('Enter a guess: '))
-    if guess_word == correct_word:
-        print('Congratulations!  You Win!')
-        is_guessed = True
-    else:
-        print('That is not the word.  Try again.')
+secretword = 'difference'
+correctletters = []
+
+for char in secretword:
+    correctletters.append('_')
+
+secretword_list = list(secretword)
+guess = str(input('Enter a guess: '))
+if guess.isalpha() == True:
+        for position in range(len(secretword_list)):
+                if secretword_list[position] == guess:
+                #print(position)
+                #print(correctword_list[position])
+                 correctletters[position] = guess
+else:
+        print('Guess a letter') 
+
+#print(correctword_list)
+print(correctletters)
+
+#    word_guessed = False
+#    while word_guessed == False:
+#        guess_word = str(input('Enter a guess: '))
+#        if guess_word == correct_word:
+#            print('Congratulations!  You Win!')
+#            word_guessed = True
+#        else:
+#            print('That is not the word.  Try again.')
+    
+#else:
+#    print('You have used all your guesses.  You lose.')
